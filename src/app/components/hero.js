40367f26db798ff751ed3a7d3e8df9fac2d73243
mateenbhatti4 <apple@Mateens-Mac.local>
2024-11-18 22:26:36 +0500

    inital commit

---
+++ src/app/components/hero.js
@@ -0,0 +1,18 @@
+export default function Hero() {
+  return (
+    <div>
+      <div className="img-dev"></div>
+      <h1 className="text-4xl mt-3 font-bold tracking-tight text-slate-200 sm:text-5xl">
+        <a href="/">Usman Abid</a>
+      </h1>
+      <h2 className="mt-3 text-lg font-medium tracking-tight text-slate-200 sm:text-xl">
+        Senior Software Engineer at InvoZone | UCP '17
+      </h2>
+      <p className="mt-4 max-w-xs leading-normal text-slate-500">
+        As a Senior Software Engineer at InvoZone, I focus on architecting
+        secure, scalable solutions and driving innovation while mentoring
+        aspiring engineers.
+      </p>
+    </div>
+  );
+}
